0aad1b41d822754e60ed9915b3b31d373ff19301
Angus Gaukroger <agaukroger77@gmail.com>
2022-11-20 20:07:59 +0000

    localhost revert

--- src/components/UserFormPage1.vue
+++ src/components/UserFormPage1.vue
@@ -60,7 +60,7 @@
     }
     onMounted(() => {
         //Gets data from backend and puts it into the store
-        axios.get('https://sweep-back.brth.uk:3000/data') //change to correct_url:3000/data
+        axios.get('https://localhost:3000/data') //change to correct_url:3000/data
         .then((response) => {
             console.log(response.data.data); 
             checkTeamsAvailable(response.data.data);
