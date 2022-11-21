abd35bcc867fa6f89f689225e8d8a6af3340b513
Hybes <ben.hybert21@gmail.com>
2022-11-21 18:49:20 +0000

    use https site on seperate server - need CORS

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import './style.css'
 
 createApp(App).use(router).use(store).use(AOS.init()).mount('#app')
